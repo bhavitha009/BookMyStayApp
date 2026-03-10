441858fbf7134635c622ab30ad8869d2c4dccb51
bhavitha009 <bhavithagari1342@gmail.com>
2026-03-10 09:53:44 +0530

    UC1: Application entry and welcome message

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Application Name : Hotel Booking System");
+        System.out.println("Version : 1.0");
 
     }
 
