629d187e1c0f36b0176f601b185b3e6012b6b595
bhavitha009 <bhavithagari1342@gmail.com>
2026-03-17 08:53:34 +0530

    UC7: Add-on service selection

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Add-On Service Selection");
 
+        String reservationId = "Single-1";
+
+        List<Integer> services = new ArrayList<>();
+
+        services.add(500);   // Breakfast
+        services.add(500);   // WiFi
+        services.add(500);   // Spa
+
+        int totalCost = 0;
+
+        for(int cost : services) {
+            totalCost += cost;
+        }
+
+        System.out.println("Reservation ID: " + reservationId);
+        System.out.println("Total Add-On Cost: " + totalCost + ".0");
     }
-
 }
